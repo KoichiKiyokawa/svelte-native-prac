8ae1d01eda2bff9068a33d40c26dad135f6644f5
K.Kiyokawa <koichi20110068@gmail.com>
2020-09-11 01:08:09 +0900

    feat: counter

--- app/App.svelte
+++ app/App.svelte
@@ -1,21 +1,66 @@
 <script lang="typescript">
-  let message: string = 'Blank Svelte Native App'
+  let selectedTab = 0
+  let count = 0
+  let textFieldValue = 'new'
 </script>
 
 <page>
-  <actionBar title="Svelte Native App" />
-  <gridLayout>
-    <label class="info" horizontalAlignment="center" verticalAlignment="middle" textWrap="true">
-      <formattedString><span class="fas" text="" /> <span text=" {message}" /></formattedString>
-    </label>
-  </gridLayout>
+  <actionBar title="Svelte Native Prac" />
+  <bottomNavigation bind:selectedIndex={selectedTab} tabsPosition="bottom">
+    <!-- The bottom tab UI is created via TabStrip (the containier) and TabStripItem (for each tab)-->
+    <tabStrip>
+      <tabStripItem>
+        <label text="Home" />
+        <image src="font://" class="fas t-24" />
+      </tabStripItem>
+      <tabStripItem class="special">
+        <label text="Account" />
+        <image src="font://" class="fas t-24" />
+      </tabStripItem>
+      <tabStripItem class="special">
+        <label text="Search" />
+        <image src="font://" class="fas t-24" />
+      </tabStripItem>
+    </tabStrip>
+
+    <!-- The number of TabContentItem components should corespond to the number of TabStripItem components -->
+    <tabContentItem>
+      <stackLayout>
+        <wrapLayout class="counter">
+          <label text={count} class="h2 text-center" />
+          <textField bind:text={textFieldValue} />
+          <button text="+1" class="btn inc" on:tap={() => count++} />
+          <button text="-1" class="btn dec" on:tap={() => count > 0 && count--} />
+        </wrapLayout>
+      </stackLayout>
+    </tabContentItem>
+    <tabContentItem>
+      <gridLayout><label text="Account Page" class="h2 text-center" /></gridLayout>
+    </tabContentItem>
+    <tabContentItem>
+      <gridLayout><label text="Search Page" class="h2 text-center" /></gridLayout>
+    </tabContentItem>
+  </bottomNavigation>
 </page>
 
 <style>
-  .info .fas {
-    color: #3a53ff;
+  .counter {
+    border-width: 1px;
+    border-color: #111111;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    border-radius: 5px;
   }
-  .info {
-    font-size: 20;
+  .btn {
+    border-radius: 5px;
+    color: #fff;
+    width: 64px;
+  }
+  .inc {
+    background-color: #269f42;
+  }
+
+  .dec {
+    background-color: #cc3300;
   }
 </style>
---
+++ app/components/Card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<gridLayout class="card">
+  <label class="card-title"> <span>Title</span> </label>
+  <label class="card-body"> <span>I am a geek.</span> </label>
+</gridLayout>
+
+<style>
+  .card {
+    width: 400px;
+  }
+  .card-title {
+    font-size: 24px;
+    height: 48px;
+  }
+</style>
